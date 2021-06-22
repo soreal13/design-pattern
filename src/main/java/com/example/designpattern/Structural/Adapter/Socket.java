0c95a2b7cfdf6eb0d97759c0023fc6b913727957
sojin.kim <sojin.kim@lemonhc.com>
2021-06-22 17:03:27 +0900

    구조 패턴 - 어댑터 패턴

---
+++ src/main/java/com/example/designpattern/Structural/Adapter/Socket.java
@@ -0,0 +1,8 @@
+package com.example.designpattern.Structural.Adapter;
+
+public class Socket {
+
+    public Volt getVolt(){
+        return new Volt(120);
+    }
+}
---
+++ src/main/java/com/example/designpattern/Structural/Adapter/SocketAdapter.java
@@ -0,0 +1,11 @@
+package com.example.designpattern.Structural.Adapter;
+
+public interface SocketAdapter {
+
+    public Volt get120Volt();
+
+    public Volt get12Volt();
+
+    public Volt get3Volt();
+
+}
